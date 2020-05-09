9a619eb9a0cea86dfb0d5252919542a26a423631
Héctor Zaragoza Arranz <hector.zaragoza.arranz@gmail.com>
2020-05-09 19:51:08 +0200

    Updated assets for unity problem

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;                //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+
+        float move = this.speed * 0.2f;
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("he pulsado D!!");
+            transform.position = new Vector2(transform.position.x + move, transform.position.y);
+        }   
+    }
+}
