7a596a30114149c9601087dd1eb619d837b28b76
Héctor Zaragoza Arranz <hector.zaragoza.arranz@gmail.com>
2020-05-10 17:39:02 +0200

    Working in section generation prototype

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+
+    public double sectionWidth;
+    public int maximumSectionsLoaded;
+
+    private List<GameObject> sections = new List<GameObject>();
+    private List<GameObject> sectionsLoaded = new List<GameObject>();
+
+    private void Awake()
+    {
+        this.LoadSections();
+    }
+
+    private void LoadSections()
+    {
+        int i = 1;
+        while (true)
+        {
+            if (!File.Exists("Assets/Resources/Prefabs/Sections/" + i + ".prefab"))
+            {
+                break;
+            }
+
+            GameObject section = (GameObject)Resources.Load("Prefabs/Sections/" + i, typeof(GameObject));
+            this.sections.Add(section);
+            i += 1;
+        }
+    }
+
+    public void AddSection(int position)
+    {
+        Debug.Log("Holaaaa");
+        int r = Random.Range(0, this.sections.Count);
+        GameObject go = this.sections[r];
+
+        float x = (float)(this.sectionWidth * position);
+       
+        go.transform.position = new Vector2(x, 0);
+        GameObject goInstantiated = Instantiate(go, go.transform.position, Quaternion.identity);
+
+        this.sectionsLoaded.Add(goInstantiated);
+
+        if(this.sectionsLoaded.Count > this.maximumSectionsLoaded)
+        {
+            GameObject goToRemove = this.sectionsLoaded[0];
+            this.sectionsLoaded.RemoveAt(0);
+            Destroy(goToRemove);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for(int i = 0; i < 10; i += 1)
+        //{
+            this.AddSection(0);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,24 +6,41 @@ public class PlayerScript : MonoBehaviour
 {
     public float speed;                //Floating point variable to store the player's movement speed.
 
-    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private List<int> idsCollisioned = new List<int>();
 
     // Use this for initialization
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D>();
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
 
-        float move = this.speed * 0.2f;
-        if (Input.GetKey(KeyCode.D))
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(moveHorizontal * this.speed, moveVertical * 0.15f, 0.0f);
+        //GetComponent<Rigidbody2D>().AddForce(new Vector2(moveHorizontal * speed, moveVertical * 0.15f));
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Collisione");
+        if(collider.gameObject.CompareTag("Section"))
         {
-            Debug.Log("he pulsado D!!");
-            transform.position = new Vector2(transform.position.x + move, transform.position.y);
-        }   
+            Debug.Log("Seccion sexy");
+            int id = collider.GetInstanceID();
+            Debug.Log("Id: " + id);
+            if (!this.idsCollisioned.Contains(id))
+            {
+                int position = this.idsCollisioned.Count + 1;
+                Debug.Log("Position: " + position);
+                this.idsCollisioned.Add(id);
+
+                GameScript sc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameScript>();
+                sc.AddSection(position);
+            }
+        }
     }
 }
