18c0fc984c7d8865209cb1587a34f40f0343f93b
David Verduin Cortés <43925788+DavidVerduin@users.noreply.github.com>
2020-05-15 18:18:50 +0200

    Added bow and projectile controller

---
+++ Assets/Scripts/BowController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowController : MonoBehaviour
+{
+
+    private GameObject arrowPrefab;
+
+    public float initialVelocity = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.arrowPrefab = (GameObject)Resources.Load("Prefabs/Arrow", typeof(GameObject));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            launchArrow();
+        }
+
+    }
+
+    void launchArrow()
+    {
+
+        GameObject newArrow = Instantiate(this.arrowPrefab) as GameObject;
+
+        newArrow.transform.position = this.transform.position + newArrow.GetComponent<BoxCollider2D>().bounds.size/2;
+
+        Rigidbody2D rb = newArrow.GetComponent<Rigidbody2D>();
+
+        Camera camera = Camera.main;
+        Vector3 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        Vector3 bowPosition = this.transform.position;
+
+        Vector3 shootDirection = (mousePosition - bowPosition).normalized;
+
+        rb.velocity = new Vector2(shootDirection.x * this.initialVelocity, shootDirection.y * this.initialVelocity);
+    }
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    #region Attributes
+
+    private Collider2D collider;
+    private Rigidbody2D rb;
+
+    #endregion Attributes
+
+
+    #region LifeCycle
+
+    private void Start()
+    {
+        this.collider = this.GetComponent<Collider2D>();
+        this.rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(this.rb.velocity != new Vector2(0,0))
+        {
+            this.transform.rotation = Quaternion.LookRotation(this.rb.velocity);
+        }
+    }
+
+    #endregion LifeCycle
+}
