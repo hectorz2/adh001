013a7a5904e976ffdd0518cb322989c5fbdd1a48
almudenaor96 <almudenaor96@gmail.com>
2020-05-25 15:38:17 +0200

    PJMove_WIP
    PJMove bug while PJ move over diagonal platform

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,27 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float speed;                //Floating point variable to store the player's movement speed.
+    public float speed;                 //Floating point variable to store the player's movement speed.
+    public float forceY;
 
+    private bool jumping = false;     
+    private Rigidbody2D rb;
     private List<int> idsCollisioned = new List<int>();
+
+    private bool facingRight = true;
+    //SpriteRenderer playerRender; spriteanimation
 
     // Use this for initialization
     void Start()
     {
+        this.rb = GetComponent<Rigidbody2D>();
+      //  playerRender = GetComponent<SpriteRenderer>(); spriteanimation
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        float moveX = 0f;
+        float moveY = 0f;
+               
+        if(Input.GetKey("d") || Input.GetKey("a")) 
+        {
+            moveX = moveHorizontal * speed;
 
-        transform.Translate(moveHorizontal * this.speed, moveVertical * 0.15f, 0.0f);
-        //GetComponent<Rigidbody2D>().AddForce(new Vector2(moveHorizontal * speed, moveVertical * 0.15f));
+            //if(facingRight == false && moveHorizontal >0) spriteanimation
+            //{
+              //  Rotation();
+            //}
+           // else
+           // {
+             //   Rotation();
+           // }
+        }
+        if(Input.GetKey("w"))
+        {           
+
+            if(!jumping)
+            {
+                Debug.Log("Saltando");
+                this.rb.AddForce(transform.up * this.forceY, ForceMode2D.Impulse);
+
+                jumping = true;
+            }
+            
+        }
+
+        transform.Translate(moveX * this.speed, 0.0f, 0.0f);
+        
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -43,4 +80,19 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground") && rb.velocity.y < 0.01)
+        {
+            Debug.Log("Suelo");
+            this.jumping = false;
+        }
+    }
+
+    //private void Rotation() spriteanimation
+   // {
+   //     facingRight = !facingRight;
+   //     playerRender.flipX = !playerRender.flipX;
+   // }
 }
